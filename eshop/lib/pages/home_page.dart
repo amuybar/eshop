386c7fc4f56a49ff7efb3ed7920bbc9c464002a5
amuybar <amuyunzubq@gmail.com>
2024-02-22 14:43:49 -0500

    Implimented Search button

--- eshop/lib/pages/home_page.dart
+++ eshop/lib/pages/home_page.dart
@@ -1,11 +1,16 @@
 // ignore_for_file: library_private_types_in_public_api, deprecated_member_use
 
+import 'dart:convert';
+
+import 'package:eshop/pages/product_detail.dart';
 import 'package:eshop/pages/widgets/cart.dart';
 import 'package:eshop/pages/widgets/fav.dart';
 import 'package:eshop/pages/widgets/home.dart';
 import 'package:eshop/pages/widgets/profile.dart';
+import 'package:eshop/service/model/product.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,11 +29,37 @@ final pages = <Widget>[
 bool isSerch = false;
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController searchController = TextEditingController();
+ List<Product> searchResults = [];
+  Future<void> fetchSearchResults(String query) async {
+    final response =
+        await http.get(Uri.parse('http://localhost:3000/products/search?query=$query'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      setState(() {
+        searchResults = data.map((json) => Product.fromJson(json)).toList();
+      });
+    } else {
+      throw Exception('Failed to load search results');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: isSerch?const Text('Eshop'):TextField(),
+        title: isSerch
+            ? const Text('Eshop')
+            : TextField(
+                controller: searchController,
+                onChanged: (value) {
+                  fetchSearchResults(value);
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                ),
+              ),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -66,7 +97,9 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: pages[index],
+      body: isSerch
+      ?pages[index]
+      :searchScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
         onTap: (value) {
@@ -96,6 +129,24 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+    );
+  }
+
+  ListView searchScreen() {
+    return ListView.builder(
+      itemCount: searchResults.length,
+      itemBuilder: (context, index) {
+        final product = searchResults[index];
+        return ListTile(
+          title: Text(product.name),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProductDetailScreen(product: product)),
+            );
+          },
+        );
+      },
     );
   }
 }
